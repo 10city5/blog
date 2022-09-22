00efc95ce4d24958a5240a5f5089d872bdd61d01
10city5 <112950079+10city5@users.noreply.github.com>
2022-09-22 14:31:43 -0700

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,18 @@
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB_mT_5Iml64IHTVvwzEeTut9nP-E2RwZ4",
+    authDomain: "blog-77882.firebaseapp.com",
+    projectId: "blog-77882",
+    storageBucket: "blog-77882.appspot.com",
+    messagingSenderId: "485835505445",
+    appId: "1:485835505445:web:bb4b2723cc8bc9ff4ce4cc"
+  };
+
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+
+  const db = firebase.firestore();
+
+ 
